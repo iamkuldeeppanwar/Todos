97fd607f829cdcc46dde53924faa767b8b8d82e7
kuldeep <kuldeep10panwar@gmail.com>
2021-06-07 04:05:56 -0700

    initial commit

---
+++ Todolist.js
@@ -0,0 +1,19 @@
+var enter = document.getElementById("btn");
+var ul = document.querySelector("ul");
+var input = document.getElementById("Item");
+function createlistitem(){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value="";
+       var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+
+        function deleteListItem(){
+                li.classList.add("delete");
+        }
+}
+enter.addEventListener("click",createlistitem);
+
