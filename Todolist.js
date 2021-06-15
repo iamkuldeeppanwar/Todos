f7263ae9e46a78e3cbfd30d108ca844ae7ecb127
kuldeep <kuldeep10panwar@gmail.com>
2021-06-15 07:30:45 -0700

    second commit

--- Todolist.js
+++ Todolist.js
@@ -1,19 +1,28 @@
 var enter = document.getElementById("btn");
 var ul = document.querySelector("ul");
 var input = document.getElementById("Item");
-function createlistitem(){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value="";
-       var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
 
-        function deleteListItem(){
-                li.classList.add("delete");
-        }
+function createlistitem(){
+var li = document.createElement("li");
+li.appendChild(document.createTextNode(input.value));
+ul.appendChild(li);
+input.value="";
+var dBtn = document.createElement("button");
+dBtn.appendChild(document.createTextNode("X"));
+li.appendChild(dBtn);
+dBtn.addEventListener("click", deleteListItem);
+function deleteListItem(){
+        li.classList.add("delete");
+ }
 }
-enter.addEventListener("click",createlistitem);
+function inputLength(){
+return input.value.length;
+}
+function addListAfterClick(){
+if (inputLength() > 0) { 
+        createlistitem();
+}
+}
+enter.addEventListener("click",addListAfterClick);
+
 
